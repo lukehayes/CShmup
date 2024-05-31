750209e23da63564b538d6fc33e89ea39fc68eba
lukehayes <lukepurplehayes@gmail.com>
2024-05-31 21:01:01 +0100

    Added start of arena allocator

--- main.c
+++ main.c
@@ -1,11 +1,41 @@
 #include "raylib.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <stddef.h>
 
 #define WIDTH  1280
 #define HEIGHT  720
 #define TITLE  "WINDOW"
 #define ENT_COUNT 10
-
 bool GAME_RUNNING = false;
+
+
+typedef struct Arena {
+    size_t size;
+    size_t capacity;
+    size_t position;
+    void* data;
+} Arena;
+
+Arena* arena_init(size_t capacity)
+{
+    Arena* arena    = malloc(sizeof(Arena));
+    arena->size     = 0;
+    arena->capacity = capacity;
+    arena->position = 0;
+    arena->data     = malloc(sizeof(size_t) * capacity);
+
+    return arena;
+}
+
+void arena_destroy(Arena* arena)
+{
+    free(arena->data);
+    free(arena);
+}
+
+
+
 
 int main() {
 
@@ -14,6 +44,12 @@ int main() {
     SetTargetFPS(60);
     SetExitKey(KEY_Q);
     SetTraceLogLevel(LOG_ALL);
+
+    Arena* a = arena_init(100);
+
+    printf("Arena Capacity: %lu \n", a->capacity);
+
+    arena_destroy(a);
 
 
     /* == LOOP ==================================================================*/
