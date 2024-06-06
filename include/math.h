c187bcea5012a29da8f7422d3ad0480d084cc892
lukehayes <lukepurplehayes@gmail.com>
2024-06-06 17:50:59 +0100

    rect_inside() and rect_overlap() is now working.

--- include/math.h
+++ include/math.h
@@ -4,14 +4,30 @@
 #include "raylib.h"
 #include <stdbool.h>
 
+/**
+* Is a rectangle completely inside of another rectangle?
+*
+* @param Rectangle a
+* @param Rectangle b
+*
+* @return bool
+*/
 bool rect_inside(Rectangle a, Rectangle b)
 {
-    return a.x > b.x &&
-           a.x + a.width < b.width + b.x &&
-           a.y > b.y &&
-           a.y + a.height < b.height + b.y;
+    return b.x > a.x &&
+           b.x + b.width < a.width + a.x &&
+           b.y > a.y &&
+           b.y + b.height < a.height + a.y;
 }
 
+/**
+* Does a rectangle overlap of another rectangle?
+*
+* @param Rectangle a
+* @param Rectangle b
+*
+* @return bool
+*/
 bool rect_overlap(Rectangle a, Rectangle b)
 {
     return a.x < b.x + b.width &&
