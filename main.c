b23b15b9daa53444f667a7d2fe363123eb12a991
lukehayes <lukepurplehayes@gmail.com>
2024-07-12 10:19:05 +0100

    Cleaned up main a little

--- main.c
+++ main.c
@@ -4,6 +4,7 @@
 #include "arena.h"
 
 #include <stdio.h>
+#include <stdlib.h>
 
 #define POLYMORPHIC(e, var_name, cls) ((e)* (var_name) = ((e)*) &cls;)
 
@@ -15,6 +16,7 @@ typedef struct Foo
     size_t value;
 } Foo;
 
+
 int main() {
 
     /* == RAYLIB INIT ============================================================*/ 
@@ -25,24 +27,7 @@ int main() {
     SetExitKey(KEY_SPACE);
     SetTraceLogLevel(LOG_ALL);
 
-    Entity ents[ENT_COUNT];
-
-    Arena* arena = arena_init(100);
-
-    int n = 111;
-    int m = 333;
-    Foo f1 = {.name = "F1", .value = 555};
-    Foo f2 = {.name = "F2", .value = 777};
-
-    size_t n_pos = arena_insert(arena, &n, sizeof(n));
-    size_t m_pos = arena_insert(arena, &m, sizeof(m));
-    arena_insert(arena, &f1, sizeof(f1));
-    arena_insert(arena, &f2, sizeof(f2));
-
-    printf("Position: %lu, Value: %lu \n\n", n_pos, *(size_t*)arena->data + n_pos);
-    printf("Position: %lu, Value: %lu \n\n", m_pos, *(size_t*)arena->data + m_pos);
-    printf("TEST: %lu \n", *(int*)arena->data);
-
+    Arena* arena = arena_init(2);
     arena_release(arena);
 
 
