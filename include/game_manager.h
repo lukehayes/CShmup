60fd334bb4a841f8c6c776c55352294aad61fba8
lukehayes <lukepurplehayes@gmail.com>
2024-07-17 17:22:55 +0100

    Added GameManager struct

---
+++ include/game_manager.h
@@ -0,0 +1,33 @@
+#ifndef GAME_GAMEMANAGER_H
+#define GAME_GAMEMANAGER_H
+
+#include "game_state.h"
+#include <stdlib.h>
+
+typedef struct GameManager
+{
+    GameState gameState;
+    size_t    lives;
+    size_t    highscore;
+    size_t    level;
+
+} GameManager;
+
+GameManager* GameManagerCreate()
+{
+    GameManager* manager = malloc(sizeof(GameManager));
+
+    manager->gameState = GAMESTATE_START_MENU;
+    manager->lives = 3;
+    manager->level = 1;
+    manager->highscore = 0;
+
+    return manager;
+};
+
+
+void GameManagerDestroy(GameManager* manager)
+{
+    free(manager);
+}
+#endif // !GAME_GAMEMANAGER_H
