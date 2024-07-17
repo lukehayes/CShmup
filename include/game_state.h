ae63763e1b457e949e3eec8abd8c3d9a4a2e9489
lukehayes <lukepurplehayes@gmail.com>
2024-07-17 17:22:43 +0100

    Added GameState enum

---
+++ include/game_state.h
@@ -0,0 +1,13 @@
+#ifndef GAME_GAMESTATE_H
+#define GAME_GAMESTATE_H
+
+typedef enum GameState
+{
+    GAMESTATE_START_MENU,
+    GAMESTATE_PAUSED,
+    GAMESTATE_PLAYING,
+    GAMESTATE_COUNT
+
+} GameState;
+
+#endif // !GAME_GAMESTATE_H
