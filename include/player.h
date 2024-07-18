682895828eb413acdb84818c29ceb86a0e940144
lukehayes <lukepurplehayes@gmail.com>
2024-07-18 15:55:35 +0100

    Added simple player movement

--- include/player.h
+++ include/player.h
@@ -21,14 +21,41 @@ Player* PlayerCreate(int x, int y, GameManager* game)
     player->transform.position.x = x;
     player->transform.position.y = y;
     player->transform.rotation   = 0;
-    player->transform.scale.x    = game->tileSize * game->tileScale;
-    player->transform.scale.y    = game->tileSize * game->tileScale;
+    player->transform.scale.x    = game->tileSize * game->tileScale * 2;
+    player->transform.scale.y    = game->tileSize * game->tileScale * 2;
 
     player->color                = GREEN;
-    player->speed                = 100;
+    player->speed                = 300;
 
     return player;
 }
+
+
+void PlayerUpdate(Player* player, GameManager* game)
+{
+    if(IsKeyDown(KEY_W))
+    {
+        player->transform.position.y -= 1 * player->speed * game->deltaTime;
+    }
+    
+    if(IsKeyDown(KEY_S))
+    {
+        player->transform.position.y += 1 * player->speed * game->deltaTime;
+    }
+
+    if(IsKeyDown(KEY_A))
+    {
+        player->transform.position.x -= 1 * player->speed * game->deltaTime;
+    }
+
+    if(IsKeyDown(KEY_D))
+    {
+        player->transform.position.x += 1 * player->speed * game->deltaTime;
+    }
+
+}
+
+
 
 void PlayerDestroy(Player* player)
 {
