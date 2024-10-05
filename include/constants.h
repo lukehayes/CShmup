2e9745424c2d334087bf5cc0a337d931b45ef13a
lukehayes <lukepurplehayes@gmail.com>
2024-10-05 11:10:20 +0100

    Window size now using multiplier

--- include/constants.h
+++ include/constants.h
@@ -1,9 +1,10 @@
 #ifndef GM_CONSTANTS_H
 #define GM_CONSTANTS_H
 
-#define WINDOW_WIDTH   (720  / 2)
-#define WINDOW_HEIGHT  (1280 / 2)
-#define TITLE          "WINDOW"
+#define MULT 4
+#define WINDOW_WIDTH   (320 * MULT)
+#define WINDOW_HEIGHT  (180 * MULT)
+#define TITLE          "Window."
 #define ENT_COUNT      10
 
 #endif // !GM_CONSTANTS_H
