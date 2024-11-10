b542f08a4350cc6813e215250c25b2ead8bd49cf
lukehayes <lukepurplehayes@gmail.com>
2024-11-10 10:03:08 +0000

    Added flashing text to menu state

--- include/levels.h
+++ include/levels.h
@@ -5,12 +5,18 @@
 #include "game_manager.h"
 #include "renderer.h"
 #include <stdio.h>
+#include <math.h>
 
 void LevelStartMenu(GameManager* game)
 {
+    static float c = 0.0;
+    c += 0.1;
+
     BeginDrawing();
         ClearBackground(BLACK);
-        DrawTextEx(game->font, "!*?;@~# ABCDE abcde 12345", (Vector2){10,10}, game->font_size * game->font_mult, 0, WHITE);
+
+        DrawTextEx(game->font, "!*?;@~# ABCDE abcde 12345", (Vector2){800.0  / 2, 600.0 / 2}, game->font_size * game->font_mult, 0, (Color){255,255,255, sin(c) * 100} );
+        //DrawTextEx(game->font, "Press Space To Start.", (Vector2){1100.0  / 2, 700.0 / 2}, game->font_size * game->font_mult, 0, (Color){255,255,255, sin(c) * 100} );
         // DrawText("Press X to start...", 10, 50, 16, WHITE);
     EndDrawing();
 }
